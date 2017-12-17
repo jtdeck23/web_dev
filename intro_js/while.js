31b73def734ab01f42a8e665c5db8494919932ef
Jordan Deck <jordandeck060@gmail.com>
2017-12-16 22:17:06 -0600

    contiued intro js section

---
+++ intro_js/while.js
@@ -0,0 +1,37 @@
+console.log('problem set 1');
+var num = -10
+
+while(num < 20) {
+  console.log(num);
+  num++;
+}
+
+console.log('problem set 2');
+var num2 = 10;
+
+while(num2 <= 40){
+  if(num2 % 2 === 0){
+    console.log(num2);
+  }
+  num2++
+}
+
+console.log('problem set 3');
+var num3 = 300;
+
+while(num3 <= 333){
+  if(num3 % 2 !== 0){
+    console.log(num3);
+  }
+  num3++
+}
+
+console.log('problem set 4');
+var num4 = 5;
+
+while(num4 <= 50){
+  if((num4 % 3 === 0) && (num4 % 5 === 0)){
+    console.log(num4);
+  }
+  num4++
+}
