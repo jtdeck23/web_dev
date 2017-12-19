015e06db6b15c5e2fba11fd93c1ff022dd94e38f
Jordan Deck <jordandeck060@gmail.com>
2017-12-18 21:40:44 -0600

    completed arrays section

--- intro_js/list.js
+++ intro_js/list.js
@@ -5,13 +5,35 @@ var input = prompt('What would you like to do?');
 while(input !== 'quit'){
 
   if(input === 'list') {
-    console.log(todos);
+    listTodos();
   } else if(input === 'new') {
-    //ask for new todos
-    var newTodo = prompt('Enter new todo');
-    //add to todos array
-    todos.push(newTodo);
+    addTodo();
+  } else if(input === 'delete'){
+    deleteTodo();
   }
   input = prompt('What would you like to do?');
 }
 console.log('YOU QUIT THE APP');
+
+function listTodos(){
+  console.log("**********");
+  todos.forEach(function(todo, i){
+    console.log(i + ": " + todo);
+  });
+  console.log("**********");
+}
+
+function addTodo(){
+  //ask for new todos
+  var newTodo = prompt('Enter new todo');
+  //add to todos array
+  todos.push(newTodo);
+  console.log('added todo');
+}
+
+function deleteTodo(){
+  var index = prompt('Enter index of todo to delete.');
+
+  todos.splice(index, 1);
+  console.log('deleted todo');
+}
---
+++ intro_js/solution.js
@@ -0,0 +1,35 @@
+console.log('CONNECTED');
+
+function printReverse(arr){
+  for(var i = arr.length - 1; i >= 0; i--){
+    console.log(arr[i]);
+  }
+}
+
+function isUniform(arr){
+  var first = arr[0];
+  for(var i = 1; i < arr.length; i++){
+    if(arr[i] !== first){
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumArray(arr){
+  var sum = 0;
+  arr.forEach(function(element){
+    sum += element;
+  });
+  return sum;
+}
+
+function max(arr){
+  var max = arr[0];
+  for(var i = 1; i < arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
